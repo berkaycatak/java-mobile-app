69a37cf3bdf9c02008255a02230bee48eb1983c5
Berkay Çatak <34205493+berkaycatak@users.noreply.github.com>
2021-01-21 13:14:55 +0300

    Gorsel eklendi

--- app/src/main/java/com/H5190058/berkay_catak_final/util/Constants.java
+++ app/src/main/java/com/H5190058/berkay_catak_final/util/Constants.java
@@ -3,6 +3,6 @@ package com.H5190058.berkay_catak_final.util;
 public class Constants {
 
     public static String TASINAN_BASLIK = "tiklananIlan";
-    public static String BASE_URL = "https://raw.githubusercontent.com/berkaycatak/KayipHayvanlar/main/api/";
+    public static String BASE_URL = "https://raw.githubusercontent.com/berkaycatak/h5190058berkaycatak/main/api/";
 
 }
